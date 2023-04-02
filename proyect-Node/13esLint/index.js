94c84204e8003951583fc42dd9caa4294c2661ab
LilAtza <ysaacnoe.correa@unmsm.edu.pe>
2023-04-01 23:10:23 -0500

    Actualizacion de teoria y Ejercicios13 Eslint

---
+++ proyect-Node/13esLint/index.js
@@ -0,0 +1,11 @@
+const dev = 'Ysaac';
+
+let name2 = 'Atza';
+
+console.log(`Hola ${dev} y ${name2}!`);
+
+//how to disable eslint in a line
+const string = 'Carlos'; 
+
+
+const string2= 'Carlos'; 
